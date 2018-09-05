fe958f8567ebd8fc1a7df3067c288bedad28fb8e
SerjRamone <sergey.greznov@gmail.com>
2018-09-05 23:20:00 +0300

    двигаем шляпу

---
+++ Assets/Scripts/HatController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HatController : MonoBehaviour
+{
+    public Camera cam;
+
+    private float maxWidth;
+
+    private void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+            Vector3 upperCorner = new Vector3(Screen.width, Screen.height, .0f);
+            Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+            float hatWidth = GetComponentInChildren<Renderer>().bounds.extents.x;
+            maxWidth = targetWidth.x - hatWidth;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        float yPos = GetComponent<Transform>().position.y;
+        Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 targetPosition = new Vector3(rawPosition.x, yPos, .0f);
+        float targetWith = Mathf.Clamp(targetPosition.x, -maxWidth, maxWidth);
+        targetPosition = new Vector3(targetWith, targetPosition.y, targetPosition.z);
+        GetComponent<Rigidbody2D>().MovePosition(targetPosition);
+    }
+}
