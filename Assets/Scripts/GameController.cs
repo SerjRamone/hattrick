4de98b3c1cbf80a307d2f411eef108066e77907f
SerjRamone <sergey.greznov@gmail.com>
2018-09-08 23:28:20 +0300

    спавн шаров

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+    public Camera cam;
+    public GameObject ball;
+
+    private float maxWidth;
+
+    private void Start()
+    {
+        if (cam == null)
+        {
+            cam = Camera.main;
+            Vector3 upperCorner = new Vector3(Screen.width, Screen.height, .0f);
+            Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+            float ballWidth = ball.GetComponent<Renderer>().bounds.extents.x;
+            maxWidth = targetWidth.x - ballWidth;
+            StartCoroutine(Spawn());
+        }
+    }
+
+    IEnumerator Spawn()
+    {
+        yield return new WaitForSeconds(2.0f);
+        while (true)
+        {
+            Vector3 spawnPosition = new Vector3(
+                Random.Range(-maxWidth, maxWidth),
+                transform.position.y,
+                .0f
+            );
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(ball, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(1.0f, 2.0f));
+        }
+    }
+}
