516a7401e51576f0f21e474de43bcda0105545c1
serjramone <sergey.greznov@gmail.com>
2018-10-04 21:34:00 +0200

    подсчёт очков

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text scoreText;
+    public int ballValue;
+
+    private int score;
+
+    private void Start()
+    {
+        score = 0;
+        UpdateScore();
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score:\n " + score;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        score += ballValue;
+        UpdateScore();
+    }
+}
